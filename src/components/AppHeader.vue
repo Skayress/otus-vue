277f93dadba627cda9b2fee4710703bda18f1260
Skayress <demangroshev@yandex.ru>
2024-01-07 09:59:12 +0400

    промежуточный коммит

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,12 @@
+<template>
+   <header class="header">
+      <strong class="logo"> OTUS SHOP </strong>
+      <div class="search-product">
+         <input
+            class="search-input"
+            type="text"
+            placeholder="Введите название товара..."
+         />
+      </div>
+   </header>
+</template>
--- src/components/ProductPage.vue
+++ src/components/ProductPage.vue
@@ -1,32 +1,45 @@
 <template>
-	<loader v-if="isLoading"></loader>
+   <app-header></app-header>
 
-	<div class="product-list">
-		<product-card
-			v-for="card in products"
-			:key="card.id"
-			:title="card.title"
-			:price="card.price"
-			:descr="card.description"
-			:category="card.category"
-			:img="card.image"
-			:rating="card.rating"
-		></product-card>
-	</div>
+   <loader v-if="isLoading"></loader>
+
+   <div class="row">
+      <h1 class="title">Все товары</h1>
+		<select>
+			<option value="">По цене (сначала дешевые)</option>
+			<option value="">По цене (сначала дорогие)</option>
+			<option value="">По названию (А-Я)</option>
+		</select>
+   </div>
+
+   <div class="product-list">
+      <product-card
+         v-for="card in products"
+         :key="card.id"
+         :title="card.title"
+         :price="card.price"
+         :descr="card.description"
+         :category="card.category"
+         :img="card.image"
+         :rating="card.rating"
+      ></product-card>
+   </div>
 </template>
 
 <script setup>
-import { ref, onBeforeMount } from 'vue';
-import ProductCard from './ProductCard.vue';
-import Loader from './Loader.vue';
-import getProducts from '../services/get-products';
+import { ref, onBeforeMount } from "vue";
+import getProducts from "../services/get-products";
 
-const products = ref(null)
-const isLoading = ref(false)
+import ProductCard from "./ProductCard.vue";
+import Loader from "./Loader.vue";
+import AppHeader from "./AppHeader.vue";
+
+const products = ref(null);
+const isLoading = ref(false);
 
 onBeforeMount(async () => {
-	isLoading.value = true
-	products.value = await getProducts()
-	isLoading.value = false
-}) 
+   isLoading.value = true;
+   products.value = await getProducts();
+   isLoading.value = false;
+});
 </script>
